1e61cbad95aa569165523843fb954c44951e0e35
mikeclise <mikeclise@gmail.com>
2012-09-04 21:15:29 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/helloworld/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Precor"
+maintainer_email "mike.clise@precor.com"
+license          "All rights reserved"
+description      "Installs/Configures helloworld"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "helloworld::default","Prints hello world output"
