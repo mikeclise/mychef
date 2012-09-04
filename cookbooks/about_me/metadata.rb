c48004dc6c7efbe0f2b36fa4456f7203a6b74120
mikeclise <mikeclise@gmail.com>
2012-09-04 23:23:20 +0000

    Converted the hobby attribute into an input

--- cookbooks/about_me/metadata.rb
+++ cookbooks/about_me/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Precor"
+maintainer_email "mike.clise@precor.com"
 license          "All rights reserved"
 description      "Installs/Configures about_me"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,3 +8,10 @@ version          "0.0.1"
 depends "rightscale"
 
 recipe "about_me::default","Prints my first name and several of my favorite things as output."
+
+# Required #
+attribute "about_me/favorite/hobby",
+   :display_name => "Hobby",
+   :description => "My favorite hobby.",
+   :required => "required",
+   :recipes => ["about_me::default"]
