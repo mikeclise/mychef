244e39eae3a58f5333991f84cbd1da5078110b5d
mikeclise <mikeclise@gmail.com>
2012-09-04 22:00:58 +0000

    Added rightscale_marker to default.rb

--- cookbooks/helloworld/metadata.rb
+++ cookbooks/helloworld/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures helloworld"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
 recipe "helloworld::default","Prints hello world output"
